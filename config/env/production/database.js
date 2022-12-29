0f4922333082914539ed65c7e953cda1d81c763a
wpayush07 <100691164+wpayush07@users.noreply.github.com>
2022-12-29 22:55:36 +0530

    Update database.js

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,19 +1,19 @@
 const { parse } = require("pg-connection-string");
 
 module.exports = ({ env }) => {
-  const { host, port, database, user, password } = parse(env("DATABASE_URL"));
+    const { host, port, database, user, password } = parse("postgres://postgres:Ayush@2007@localhost:5432/postgres");
 
-  return {
-    defaultConnection: "default",
-    connection: {
-      client: "postgres",
-      connection: {
-        host,
-        port,
-        database,
-        user,
-        password,
-      },
-    },
-  };
+    return {
+        defaultConnection: "default",
+        connection: {
+            client: "postgres",
+            connection: {
+                host,
+                port,
+                database,
+                user,
+                password,
+            },
+        },
+    };
 };
